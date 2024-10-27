5b3cecc0bee97a39fc05783342818cff70d3f79e
Роман Козарь <pozitr0n.kozarroman@gmail.com>
2024-10-27 15:14:22 +0100

    Initial Commit

---
+++ RunRhythm/RunRhythmApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunRhythmApp.swift
+//  RunRhythm
+//
+//  Created by Raman Kozar on 27/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct RunRhythmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
