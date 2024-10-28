5224697a7d5d895d7738be4b57085ba8eba94ade
Роман Козарь <pozitr0n.kozarroman@gmail.com>
2024-10-28 01:53:18 +0100

    Start of the new project: RunRhythm (sport application). Start of creating MVP UI on SwiftUI.

---
+++ RunRhythmWatch Watch App/RunRhythmWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunRhythmWatchApp.swift
+//  RunRhythmWatch Watch App
+//
+//  Created by Raman Kozar on 27/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct RunRhythmWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
