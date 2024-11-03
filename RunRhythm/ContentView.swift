2a3c973078750815ed04d3ce7fe4acba46efd89a
Роман Козарь <pozitr0n.kozarroman@gmail.com>
2024-11-03 23:41:05 +0100

    Changes at Ring Progress at RunnerSpeedometerView

--- RunRhythm/ContentView.swift
+++ RunRhythm/ContentView.swift
@@ -167,9 +167,16 @@ struct RunnerSpeedometerView: View {
             
             // Ring Progress
             Circle()
-                .trim(from: 0.0, to: min(CGFloat(currentSpeed / 12), 1.0))
-                .stroke(AngularGradient(gradient: Gradient(colors: [.green, .yellow, .red]),
-                                        center: .center), lineWidth: 20)
+                .trim(from: 0.05, to: min(CGFloat(currentSpeed / 20), 0.95)) 
+                .stroke(AngularGradient(gradient: Gradient(stops: [
+                    .init(color: Color.green.opacity(0.0), location: 0.0),
+                    .init(color: Color.green, location: 0.2),
+                    .init(color: Color.green, location: 0.4),
+                    .init(color: Color.yellow, location: 0.5),
+                    .init(color: Color.red, location: 0.6),
+                    .init(color: Color.red, location: 0.7),
+                    .init(color: Color.red.opacity(0.0), location: 1.0)
+                ]), center: .center), lineWidth: 20)
                 .rotationEffect(Angle(degrees: -90))
                 .animation(.easeInOut, value: currentSpeed)
             
@@ -190,7 +197,7 @@ struct RunnerSpeedometerView: View {
             
             // Emulate tempo change
             Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
-                self.currentSpeed = Double.random(in: 0...25)
+                self.currentSpeed = Double.random(in: 0...20)
             }
             
         }
