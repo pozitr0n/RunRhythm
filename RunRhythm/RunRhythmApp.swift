1701ca5601ecc1f67481122672ea64e17b42e692
Роман Козарь <pozitr0n.kozarroman@gmail.com>
2025-02-09 22:42:27 +0100

    Start changes: added files

---
+++ RunRhythm/RunRhythmApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunRhythmApp.swift
+//  RunRhythm
+//
+//  Created by Raman Kozar on 09/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct RunRhythmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
